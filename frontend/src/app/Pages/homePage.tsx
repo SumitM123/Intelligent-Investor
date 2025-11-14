217424a471c5789e834efff759b15b8d39502516
SMantri05 <sumit@instaservice.com>
2025-11-14 11:04:13 -0800

    creating the homepage

---
+++ frontend/src/app/Pages/homePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+ 
+function HomePage() {
+
+    return (
+        <>
+            <h1>
+                Welcome to Productivity-Assisstant
+            </h1>
+            <button>
+                Sign-in to continue
+            </button>
+        </>
+    );
+}
+
+export default HomePage;
