11a0f34a6fb0597cb42f7501124d339738faa9f3
SMantri05 <sumith.mantri@gmail.com>
2025-12-27 12:29:28 -0800

    Able to add stocks to database

--- backend/routes/stocks.py
+++ backend/routes/stocks.py
@@ -3,12 +3,14 @@ from fastapi import Request
 from pydantic import BaseModel
 from fastapi import Response, status
 import requests
-
+from database import SessionLocal
+from frequenty_used_methods import get_user_id
 router = APIRouter(prefix="/stocks")
-
+class User(BaseModel):
+    google_id: str = None
 @router.get('getStocks')
-def get_stocks(user_id: str = None):
-    if user_id == None:
+def get_stocks(user: User = None, ):
+    if user.google_id == None:
         return {"Error": "There's no user ID that's bounded to the request"}
     '''
         Check if the user exists within the database in postgreSQL. If exists, then get their stocks
@@ -16,7 +18,16 @@ def get_stocks(user_id: str = None):
         You have a Users table. The user's table consists of the user_ID, and a header of stocks
             The stocks will be of type 'jsonb' datatype
         Stocks table: Each row consists of the stock name, the date of bought, how much bought, total amount bount, the price bought at, total dividends earned
-        
     '''
+    user_id = get_user_id()
+    if user_id == None:
+        return JSONResponse()
+    with SessionLocal() as session:
+        result = session.execute(text("SELECT user_id FROM users_id WHERE google_id = :google_id"), 
+                        {"google_id": stock.google_id})
+        user_id = result.google_id
+
+        
+
 
     
