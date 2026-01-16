3f3ec175e6af5e4afbab51b657d3c9e542d9f366
SumitM123 <sumith.mantri@gmail.com>
2026-01-16 14:41:46 -0800

    Splitting navbar into server and client components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <div>
+        
+      </div>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
