eb077e58a1215d7939799bc23d545cc58810574f
SumitM123 <sumith.mantri@gmail.com>
2026-01-21 22:34:08 -0800

    Using server actions to change props of signin component

---
+++ frontend/src/app/actions.tsx
@@ -0,0 +1,23 @@
+'use server'
+
+export async function passSignInProps(formData: FormData) {
+    // Make a get userID request, and if doesn't exist, make a postID request. 
+    const userGoogleID = formData.get("googleID");
+    interface RequestInterface {
+        googleID: FormDataEntryValue
+    }
+
+    const getRequestParams : RequestInterface = {
+        googleID: userGoogleID?.toString() || ''
+    };
+    const url = new URL("http://backend:8000/api/users/getUserID/");
+
+    // Ensure the value is a string (use empty string if null/File)
+    const idValue = typeof getRequestParams.googleID === 'string' ? getRequestParams.googleID : '';
+    url.searchParams.set("googleID", idValue);
+
+    let checkUserExists = await fetch(url.toString());
+    
+
+    
+}
