0a68d525ccf113a9106d3d87215ae2c4883e7f6f
SumitM123 <sumith.mantri@gmail.com>
2026-01-22 19:02:18 -0800

    Server action for props completed. And delete profilePictureKey because URL is public

--- frontend/src/app/component/SignIn/signIn.tsx
+++ frontend/src/app/component/SignIn/signIn.tsx
@@ -21,43 +21,19 @@ import { useState } from "react";
             revalidatePath() can be used to bypass the session
         
 */
-// interface SignedIn {
-//     isSignedIn: boolean;
-//     profilePicture: string; // it'll be the key of the object stored in the s3 bucket or you can serialize the file by turning it into a base64 encoder
-//     userName: String;
-// }
-
-// 'use client'
-// export default function SignIn({isSignedIn, profilePicture, userName} : SignedIn) {
-//     // default values if not signed in
-//     const [name, setName] = useState<string>(sessionStorage.getItem("userName") | "Sign In");
-//     const [userImage, setUserImage] = userState<File | null> (null);
-//     useEffect(() => {
-//         setName(sessionStorage.getItem("userName") | "Sign In");
-//         setUserImage(sessionStorage.getItem("userImage"));
-//     }, []);
-//     useEffect(() => {
+interface SignedIn {
+    isSignedIn: boolean;
+    profilePicture: string; // it'll be the key of the object stored in the s3 bucket or you can serialize the file by turning it into a base64 encoder
+    userName: string;
+}
+'use client'
+export default function SignIn({isSignedIn, profilePicture, userName} : SignedIn) {
+    // default values if not signed in
+    const [name, setName] = useState<string>(userName || "Sign In");
+    const [userImage, setUserImage] = useState<File | null> (null);
+    return (
+        <h1>
         
-
-
-//     }, [isSignedIn]);
-//     return (
-//         <h1>
-            
-//         </h1>
-//     );
-
-// }
-/*
-    heuristic = 0
-    while state(n) != goal:
-        blank_coordinates = get_blank(state(n))
-        misplaced_tile = getAnyMisplacedTile(state(n))
-        swap(blank_coordinates, misplaced_tile)
-        heuristic += 1
-        if state(n) == goal:
-            return heuristic
-        swap(findRightTileForBlankCoordiate(blank_coordinates, goal), blank_coordinate)
-        heuristic += 1
-    return heuristic
-*/
+        </h1>
+    );
+}
