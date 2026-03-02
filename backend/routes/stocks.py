18db16fb32cea13b98f7996235b23ecc007f5217
SumitM123 <sumith.mantri@gmail.com>
2026-03-02 13:54:59 -0800

    Request to Alpha for search

--- backend/routes/stocks.py
+++ backend/routes/stocks.py
@@ -29,9 +29,9 @@ class User(BaseModel):
     #     user_id = result.google_id
 
         
-@router.get("/searchStocks")
-async def search_stocks(stocks: str = ""):
-    # Make request to the search bar for update
-    
+# @router.get("/searchStocks")
+# async def search_stocks(stocks: str = ""):
+#     Make request to the search bar for update
+
 
     
