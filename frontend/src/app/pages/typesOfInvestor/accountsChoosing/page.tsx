4be6c75ab8d90c179e4678c54420a603db0f087c
SumitM123 <sumith.mantri@gmail.com>
2026-03-12 23:48:15 -0700

    Created the route, and got all the relevant accounts

--- frontend/src/app/pages/typesOfInvestor/accountsChoosing/page.tsx
+++ frontend/src/app/pages/typesOfInvestor/accountsChoosing/page.tsx
@@ -7,21 +7,26 @@
 import { usePrevPageContext } from "@/app/context/prevPageURL";
 import { useEffect, useState } from "react";
 function listAllAccounts() {
-    // useEffect( () => {
-    
-    // }, []);
     const [allAccounts, setAllAccounts] = useState([]);
-    //
     //contains the prevPageURL and the connectionID
     const accountListingContext = usePrevPageContext();
-
     useEffect(() => {
         // Get all the accounts from all the connections
-        const gettingAllAccounts = async () => {
+        const gettingRelevantAccounts = async () => {
             try {
-                const 
+                const params = new URLSearchParams();
+                params.append("connection_id", accountListingContext.connectionID);
+                const response = await fetch(`http://backend:8000/api/snapTrade/getAllAccountsFromConnection?${params}`, {
+                    credentials: "include"
+                });  
+                const resJSON = await response.json();
+                setAllAccounts(resJSON);
+            } catch (err) {
+                console.error("Error getting the accounts to the relevant connection" + (err as Error).message)
             }
         }
+        void gettingRelevantAccounts();
+
     }, []);
 }
 export default listAllAccounts;
