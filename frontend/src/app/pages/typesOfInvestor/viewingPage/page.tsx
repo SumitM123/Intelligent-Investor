7149144fefc2fadd96707395ab0d42f461d68e63
SumitM123 <sumith.mantri@gmail.com>
2026-04-12 20:44:49 -0700

    Created the getDividends route

--- frontend/src/app/pages/typesOfInvestor/viewingPage/page.tsx
+++ frontend/src/app/pages/typesOfInvestor/viewingPage/page.tsx
@@ -43,7 +43,7 @@ export default async function ViewingPage({ searchParams }: ViewingPageProps) {
 			accountError = error instanceof Error ? error.message : "Failed to fetch account information";
 		}
 	} else {
-        
+		
     }
 
 	return (
